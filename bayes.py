6d314503d337cf4dc5c7973cffabcc1bccb263fa
orlov <evgenyorlov1@gmail.com>
2017-02-14 07:23:24 +0200

    Done

--- bayes.py
+++ bayes.py
@@ -1,59 +1,55 @@
 # coding: utf8
-import csv
+from __future__ import division  # integer to floating point division
 import string
 import nltk
 
-traindata = 'data.txt'
-inputdata = 'input.txt'
-matrix = dict()
-instances = list()  # number of instances per class
+matrix = dict()  # word matrix
+sentences = list()  # P(c) - class probability
 V = 0  # vocabulary
 c = 0  # number of classes
-N = 0  # number of classes instances
+N = 0  # number of sentences
 
 
 def train(data):
     # {Wi:[N1,N2,N3]} -> ci -- count of word in class ci
-    global c, V, N, instances
+    global c, V, N, sentences
     for row in data:
         N += 1
         if c < int(row[1]): c = int(row[1])
-    instances = [0]*c
+        sentences = [0]*c
     for row in data:
-        instances[int(row[1])-1] += 1
+        sentences[int(row[1]) - 1] += 1
         for word in row[0]:
             if word not in matrix:
                 matrix.update({word: [0]*c})
                 matrix[word][int(row[1])-1] += 1
             else:
                 matrix[word][int(row[1])-1] += 1
+    for i in xrange(c):
+        sentences[i] = float(sentences[i]/N)
     V = len(matrix)
-#    for key in matrix.keys():
-#        print key, matrix[key]
 
 
 def classify(data):
-    # [[w1,w2,w3], [w4,w5,w6]]
-    global matrix, instances, N, V, c
-    translate_table = dict((ord(char), None) for char in string.punctuation)
-    with open(data) as file:
-        raw = csv.reader(file, delimiter =';')  # [sentence, sentence, sentence ...]
-        raw = [tmp[0].decode('utf-8').lower() for tmp in raw]
-        raw = [tmp.translate(translate_table) for tmp in raw]
-        raw = [nltk.word_tokenize(tmp) for tmp in raw]
-        for row in raw:
-            sentence_probability = 0
-            for cls in xrange(c):
-                unique_words = unique_words_count(cls)
-                class_probability = float(instances[cls]/N)
-                for word in row:
-                    if word in matrix:
-                        class_probability *= (matrix[word][cls] + 1)/(unique_words + V)
-                    else:
-                        class_probability *= 1/(unique_words + V)
-                if sentence_probability < class_probability:
-                    sentence_probability = class_probability
-                    row.append(cls)
+    # [[w1,w2,w3], [class], [classified],
+    # [w4,w5,w6], [class], [classified]]
+    global matrix, sentences, N, V, c
+    for i, row in enumerate(data):
+        sentence_probability = 0  # P(wi|cj) - saves heightest sentence probability for class
+        type = 0  # sentence class
+        for cls in xrange(c):
+            unique_words = unique_words_count(cls)  # number of unique words per class
+            class_probability = float(sentences[cls])  # P(c)
+            for word in row[0]:
+                if word in matrix:
+                    class_probability *= (matrix[word][cls] + 1)/(unique_words + V)
+                else:
+                    class_probability *= 1/(unique_words + V)
+            if sentence_probability < class_probability:
+                sentence_probability = class_probability
+                type = cls + 1
+        data[i].append(type)
+    return data
 
 
 def unique_words_count(cls):
@@ -66,27 +62,35 @@ def unique_words_count(cls):
 
 def load(data):
     # [[w1,w2,w3,w4],[class],
-    # [w1,w2,w3,w4],[class]
-    # ...]
-    # ham - 0
-    # spam - 1
+    # [w1,w2,w3,w4],[class]]
+    # ham - 1
+    # spam - 2
     translate_table = dict((ord(char), None) for char in string.punctuation)
     with open(data) as file:
         raw = file.readlines()
-        #raw = [tmp.strip() for tmp in raw]
         raw = [tmp.decode('utf-8').lower().strip() for tmp in raw]
         raw = [tmp.translate(translate_table) for tmp in raw]
         raw = [nltk.word_tokenize(tmp) for tmp in raw]
         raw = [[tmp, 0] for tmp in raw]
         for i, row in enumerate(raw):
             if row[0][0] == 'ham':
-                raw[i][1] = 0
-            if row[0][0] == 'spam':
                 raw[i][1] = 1
+            if row[0][0] == 'spam':
+                raw[i][1] = 2
             raw[i][0].pop(0)
         return raw
 
-load_2('SMSSpamCollection.txt')
-#data = load('data.csv')
-#train(data)
-#classify('input.csv')
+
+def preciseness(data):
+    N = 0
+    for row in data:
+        if row[1] == row[2]:
+            N += 1
+    print 'Preciseness: {}%'.format(N*100/len(data))
+
+
+data = load('SMSSpamCollection.txt')
+train(data)
+data = load('input.txt')
+data = classify(data)
+preciseness(data)
